0be12a4eb0511ac65c6820b1efdf34b18a54ddf1
jihanstkm <jihanstk@gmail.com>
2023-10-19 00:59:55 +0600

    test

--- src/Components/Content/OurDoctors/OurDoctors.jsx
+++ src/Components/Content/OurDoctors/OurDoctors.jsx
@@ -22,7 +22,7 @@ const OurDoctors = () => {
             <p className="my-4 text-sky-900">
               With the online doctor portal, your specialists will be able to
               collect patient information and prescribe digital prescriptions in
-              a simple and secure way.
+              a simple and secure way
             </p>
             <ul className="">
               <li className="flex gap-2 my-3">
