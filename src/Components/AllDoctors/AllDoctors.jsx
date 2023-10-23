70c69421de7bbd9bafb3a8d2df8f419fe4891b85
Naharun <naharunsraboni@gmail.com>
2023-10-23 19:46:59 +0600

    created doctor route

--- src/Components/AllDoctors/AllDoctors.jsx
+++ src/Components/AllDoctors/AllDoctors.jsx
@@ -1,21 +1,22 @@
-// 'use client';
-// import { useEffect, useState } from 'react';
-// import Doctors from '../Doctors/Doctors';
+'use client';
+import { useEffect, useState } from 'react';
+import Doctors from '../Doctors/Doctors';
 
-// const AllDoctors = () => {
-//     const[allDoctors, setAllDoctors] = useState([])
-//     useEffect(() =>{
-//         fetch('doctors.json')
-//         .then(res =>res.json())
-//         .then(data =>  setAllDoctors(data))
-//     },[]);
-//     return (
-//         <div>
-//             {
-//                 allDoctors.map(allDoctor =><Doctors key={allDoctor.id} allDoctor ={allDoctor}></Doctors>)
-//             }
-//         </div>
-//     );
-// };
+const AllDoctors = () => {
+    const[allDoctors, setAllDoctors] = useState([])
+    useEffect(() =>{
+        fetch('doctors.json')
+        .then(res =>res.json())
+        .then(data =>  setAllDoctors(data))
+    },[]);
 
-// export default AllDoctors;
+    return (
+        <div>
+            {
+                allDoctors.map(allDoctor =><Doctors key={allDoctor.id} allDoctor ={allDoctor}></Doctors>)
+            }
+        </div>
+    );
+};
+
+export default AllDoctors;
--- src/Components/Doctors/Doctors.jsx
+++ src/Components/Doctors/Doctors.jsx
@@ -1,19 +1,49 @@
+import { FcFeedback, FcGraduationCap, FcLike } from 'react-icons/fc';
+import { GrUserExpert } from 'react-icons/gr';
+import { FaUserDoctor } from 'react-icons/fa6';
+import { FaHospital, FaRegStar, FaStar } from 'react-icons/fa';
+import { PiCurrencyDollarFill } from 'react-icons/pi';
+import { ImLocation2 } from 'react-icons/im';
+import Rating from 'react-rating';
 
 
-// const Doctors = ({allDoctor}) => {
-//     const {image,name,study,specialist,position,place,rating,} = allDoctor;
-//     console.log(allDoctor);
-//     return (
-//         <div>
-//             <img src={image} alt="" />
-//             <p>{name}</p>
-//             <p>{study}</p>
-//             <p>{specialist}</p>
-//             <p>{position}</p>
-//             <p>{place}</p>
-//             <p>{rating}</p>
-//         </div>
-//     );
-// };
 
-// export default Doctors;
+
+const Doctors = ({ allDoctor }) => {
+    const { image, name, study, specialist, position, place, rating, price } = allDoctor;
+
+    return (
+        <div className='grid grid-cols-1 row lg:grid-cols-3 mx-auto card m-3 bg-base-200'>
+            <figure>
+                <img className='w-3/4 p-4 shadow-lg rounded-lg' src={image} alt="" />
+            </figure>
+            <div className='mx-4 my-4'>
+                <p className="font-bold text-black text-xl">{name}</p>
+                <p className='flex gap-3'><span className='text-2xl'><FcGraduationCap /></span>{study}</p>
+                <p className='flex gap-3'><span className='text-xl'><GrUserExpert /></span>{specialist}</p>
+                <p className='flex gap-3'><span className='text-xl'><FaUserDoctor /></span>{position}</p>
+                <p className='flex gap-3'><span className='text-xl'><FaHospital /></span>{place}</p>
+                <p className='flex items-center gap-2'><Rating
+                    placeholderRating={rating}
+                    emptySymbol={<FaRegStar />}
+                    placeholderSymbol={<FaStar />}
+                    fullSymbol={<FaStar />}
+                /><span className=''>{rating}</span></p>
+            </div>
+            <div className='w-1/2 mx-auto lg:mt-3'>
+                <div>
+                    <h2 className='flex items-center gap-3'><span className='text-xl'><FcLike /></span> 90%</h2>
+                    <h2 className='flex items-center gap-3'><span className='text-xl'><FcFeedback /></span> 20 Feedback</h2>
+                    <h2 className='flex items-center gap-3'><span className='text-xl'><PiCurrencyDollarFill /></span>${price}</h2>
+                    <h2 className='flex items-center gap-3'><span className='text-xl'><ImLocation2 /></span></h2>
+                </div>
+                <div className='flex flex-col my-3 '>
+                    <button className='btn btn-accent  btn-xs my-2'>View Profile</button>
+                    <button className='btn btn-primary btn-xs'>Book Appointment</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Doctors;
