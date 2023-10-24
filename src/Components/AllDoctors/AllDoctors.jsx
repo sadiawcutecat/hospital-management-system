2eabc9768820300d9ddab43bd5ab018a95b763ed
Naharun <naharunsraboni@gmail.com>
2023-10-24 13:53:19 +0600

    added doctors

---
+++ src/Components/AllDoctors/AllDoctors.jsx
@@ -0,0 +1,22 @@
+'use client';
+import { useEffect, useState } from 'react';
+import Doctors from '../Doctors/Doctors';
+
+const AllDoctors = () => {
+    const[allDoctors, setAllDoctors] = useState([])
+    useEffect(() =>{
+        fetch('doctors.json')
+        .then(res =>res.json())
+        .then(data =>  setAllDoctors(data))
+    },[]);
+
+    return (
+        <div className='bg-base-100'>
+            {
+                allDoctors.map(allDoctor =><Doctors key={allDoctor.id} allDoctor ={allDoctor}></Doctors>)
+            }
+        </div>
+    );
+};
+
+export default AllDoctors;
