fb0dea6bc55fca42449525b97be12c364c20cf58
jihanstkm <jihanstk@gmail.com>
2023-10-25 00:13:49 +0600

    fix all doctors page in navigation

--- src/app/(with-navbar)/page.js
+++ src/app/(with-navbar)/page.js
@@ -1,9 +1,7 @@
-import AllDoctors from "@/Components/AllDoctors/AllDoctors";
 import Appointment from "@/Components/Appointment/Appointment";
 import BannerSlider from "@/Components/Banner/BannerSlider";
 import OurDoctors from "@/Components/Content/OurDoctors/OurDoctors";
 import OurTest from "@/Components/Content/OurTest/OurTest";
-// import Dashboard from "@/Components/Dashboard/Dashboard";
 import PopularDoctors from "@/Components/PopularDoctors/PopularDoctors";
 import Testimonial from "@/Components/Testimonial/Testimonial";
 
@@ -11,9 +9,7 @@ export default function Home() {
   return (
     <main>
       <BannerSlider></BannerSlider>
-      {/* <Dashboard></Dashboard> */}
       <OurDoctors />
-      <AllDoctors></AllDoctors>
       <OurTest />
       <PopularDoctors></PopularDoctors>
       <Appointment></Appointment>
