6cf2880bf4cc5f55327dd5737c4a8c1a8536ed95
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-10-24 15:12:03 +0600

    dashboard

--- src/Components/hooks/useAuth.js
+++ src/Components/hooks/useAuth.js
@@ -1,4 +1,4 @@
-import { AuthContext } from '@/app/context/AuthContext';
+import { AuthContext } from '@/app/(WithNavbar)/context/AuthContext';
 import { useContext } from 'react';
 
 const useAuth = () => {
--- src/app/(WithNavbar)/page.js
+++ src/app/(WithNavbar)/page.js
@@ -2,12 +2,14 @@ import Appointment from "@/Components/Appointment/Appointment";
 import BannerSlider from "@/Components/Banner/BannerSlider";
 import OurDoctors from "@/Components/Content/OurDoctors/OurDoctors";
 import OurTest from "@/Components/Content/OurTest/OurTest";
+// import Dashboard from "@/Components/Dashboard/Dashboard";
 import Testimonial from "@/Components/Testimonial/Testimonial";
 
 export default function Home() {
   return (
     <main>
       <BannerSlider></BannerSlider>
+      {/* <Dashboard></Dashboard> */}
       <OurDoctors />
       <OurTest />
       <Appointment></Appointment>
