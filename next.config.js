731327ad6980c8442fe55c4f143db02eb5be7d52
jihanstkm <jihanstk@gmail.com>
2023-11-11 01:13:41 +0600

    added send email successfully

--- next.config.js
+++ next.config.js
@@ -10,16 +10,22 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "i.ibb.co",
-      }, 
+      },
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-      },{
+      },
+      {
         protocol: "https",
         hostname: "scontent.fdac41-1.fna.fbcdn.net",
-      },{
+      },
+      {
         protocol: "https",
         hostname: "scontent.fdac155-1.fna.fbcdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
       },
     ],
   },
